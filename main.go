c5fab1967895403da9a5ec6b91bff54afe591b89
Thales Melo <talls.urin@hotmail.com>
2024-06-16 01:46:06 -0300

    Create a PostgreSQL database schema script including the necessary table and indices for storing the person entities with attributes as specified in the requirements.

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"net/http"
-	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("API is running"))
+		w.Write([]byte("Hello, world!"))
 	})
 
 	http.ListenAndServe(":8080", nil)
