ddba511352b7a2df8fe1e03377e50288d5f9ba62
Thales Melo <talls.urin@hotmail.com>
2024-06-16 01:45:29 -0300

    Create a Dockerfile for the Go application, specifying Go 1.22.4 as the base image, and compile the main.go into a standalone executable.

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("API is running"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
